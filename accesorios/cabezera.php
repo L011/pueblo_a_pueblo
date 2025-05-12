3d4945e843cb00df66c6ee1ebdccc287ff8c8ed8
Luis Aguilar <aguilarvzla2@gmail.com>
2025-05-12 01:55:35 -0400

    Modulo: Formulario escuela

--- accesorios/cabezera.php
+++ accesorios/cabezera.php
@@ -4,7 +4,8 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="/accesorios/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="accesorios/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="styles.css">
     <title>Advanced Search Form</title>
+    <script type="text/javascript" src="accesorios/bootstrap.js"></script>
 </head>

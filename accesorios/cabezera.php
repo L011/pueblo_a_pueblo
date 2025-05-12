997b41ca348b74eafe6892b2b6fedc91566311cd
Juanxrpana <jo.hw722@gmail.com>
2025-05-12 01:45:11 -0400

    si
    si

---
+++ accesorios/cabezera.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/accesorios/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>Advanced Search Form</title>
+</head>
--- vista/inicio.php
+++ vista/inicio.php
@@ -1,2 +1,54 @@
-<?php
-echo "<h1>Hola desde inicio.php</h1>";
+<?php require_once "./accesorios/cabezera.php"; ?>
+
+<body>
+    <div class="container" id="advanced-search-form">
+        <h2>Advanced Search</h2>
+        <form>
+            <div class="form-group">
+                <label for="first-name">escuela</label>
+                <input type="text" class="form-control" placeholder="nombre" id="first-name">
+            </div>
+            <div class="form-group">
+                <label for="last-name">direccion</label>
+                <input type="text" class="form-control" placeholder="direccion" id="last-name">
+            </div>
+            <div class="form-group">
+                <label for="country">Country</label>
+                <input type="text" class="form-control" placeholder="Country" id="country">
+            </div>
+            <div class="form-group">
+                <label for="number">Phone number</label>
+                <input type="text" class="form-control" placeholder="Phone number" id="number">
+            </div>
+            <div class="form-group">
+                <label for="age">Age</label>
+                <input type="text" class="form-control" placeholder="Age" id="age">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" placeholder="Email" id="email">
+            </div>
+            <div class="form-group">
+                <label for="category">Category</label>
+                <input type="text" class="form-control" placeholder="Category" id="category">
+            </div>
+            <div class="form-group">
+                <label for="education">Education</label>
+                <input type="text" class="form-control" placeholder="Education" id="education">
+            </div>
+            <div class="form-group">
+                <label>Gender</label>
+                <div class="radio">
+                    <label class="radio-inline">
+                        <input type="radio" name="optradio">Male</label>
+                    <label class="radio-inline">
+                        <input type="radio" name="optradio">Female</label>
+                </div>
+            </div>
+            <div class="clearfix"></div>
+            <button type="submit" class="btn btn-info btn-lg btn-responsive" id="search"> <span class="glyphicon glyphicon-search"></span> Search</button>
+        </form>
+    </div>
+</body>
+
+</html>
